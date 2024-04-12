838ca01cf7116747b980f298cf5027ea2810ee15
Md Moniruzzaman Lelin <lelin171046@gmail.com>
2024-04-12 20:43:31 +0600

    add more function

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Blogscard from "../Component/Cards/Blogscard";
 import { getBlogs, deleteBlog} from "../utilites";
 import Emtystate from "../EmtyBook/Emtystate";
+import { Link } from "react-router-dom";
 
 
 
@@ -18,7 +19,12 @@ const Bookmarks = () => {
         setBlogs(storedBlogs)
 	}
 
-    if(blogs.length < 1 ) return <div className="flex flex-col justify-center items-center  min-h-[calc(100vh-116px)]">No Bookmark found</div>  
+    if(blogs.length < 1 ) return <div className="flex flex-col gap-5 justify-center items-center  min-h-[calc(100vh-116px)]"> <h2>No Bookmark found</h2>
+    <Link to='/blogs' className="relative inline-block px-4 py-2 font-medium group">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover bg-yellow-100"></span>
+                <span className="relative text-black group-hover:text-black">Read Blogs</span>
+              </Link></div>  
     return (
         <div className="grid px-4 sm:px-8 lg:px-12 py-8 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
 			{
